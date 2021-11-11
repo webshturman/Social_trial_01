a360c9580e9ec40285ff845c2607664e7f4f0137
KirillProgMaster <kirill.zarya@inbox.ru>
2021-11-11 23:30:48 +0300

    add api

---
+++ my-app/src/api/user-api.ts
@@ -0,0 +1,9 @@
+
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials:true,
+    headers: {
+        'API-KEY': 'b8b0c036-4edd-4913-8c7f-79ccaeace603'
+    },
+})
